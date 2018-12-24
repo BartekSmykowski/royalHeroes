81a20360267d0d2c57c080bd29b9229103fd0483
BartekSmykowski <bsmykowski@wp.pl>
2018-12-24 03:02:00 +0100

    init commit

---
+++ desktop/src/com/barteksmykowski/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.barteksmykowski.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.barteksmykowski.Rpg;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Rpg(), config);
+	}
+}
